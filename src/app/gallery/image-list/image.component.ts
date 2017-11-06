9e4c9a2242a06234c16fddaac444ffc1797b5411
Raúl Nimo <rauibertnf@gmail.com>
2017-11-06 22:19:30 +0100

    Separando lógica de contenido

--- src/app/gallery/image-list/image.component.ts
+++ src/app/gallery/image-list/image.component.ts
@@ -3,7 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
-  styles: []
+  styles: [`
+    img{
+        -webkit-box-shadow: 3px 4px 19px -1px rgba(0, 0, 0, 0.75);
+        -moz-box-shadow: 3px 4px 19px -1px rgba(0, 0, 0, 0.75);
+        box-shadow: 3px 4px 19px -1px rgba(0, 0, 0, 0.75);
+
+        margin-bottom: 20px;
+    }
+
+    img:hover{
+        filter: gray;
+        -webkit-filter: grayscale(1);
+    }
+  `]
 })
 export class ImageComponent implements OnInit {
 
